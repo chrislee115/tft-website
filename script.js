27d92268eac8e3c07a5fa45b8b40f416cdd3502a
Chris Lee <chrisree@umich.edu>
2019-08-22 19:27:17 -0400

    buttons for wheel

--- script.js
+++ script.js
@@ -38,7 +38,48 @@ function dragDrop() {
     this.append(champs);
 }
 //item functions
-function toggleDiv(divId) {
-    $("#"+divId).toggle();
-    $('.toggle').not($("#"+divId)).hide();
-} 
+function ItemSelect(divId) {
+    var top = -150;
+    var left = -75;
+    //skip the first element bc its the clicker
+    var targets = document.getElementsByClassName("menu")[1].querySelectorAll('.btn');
+    console.log(targets.length);
+    for (var i = 0; i < targets.length - 1; ++i) {
+        if (document.getElementById(divId).checked == false) {
+            targets[i].style.top = "0px";
+            targets[i].style.left = "0px";
+            targets[i].style.opacity = 0;
+            targets[i].style.filter = "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
+            continue;
+        }
+        switch((i + 1) % 4) {
+            case 0: 
+                if (i > 4) { 
+                    top -= 95;
+                }
+                else {
+                    top += 95;
+                }
+                left /= 2;
+                break;
+            case 1: 
+                left *= -1;
+                break;
+            case 2: 
+                if (i > 4) { 
+                    top -= 95;
+                }
+                else {
+                    top += 95;
+                }
+                left *= 2;
+                break;
+            case 3: 
+                top *= -1;
+        }
+        targets[i].style.top = top + "px";
+        targets[i].style.left = left + "px";
+        targets[i].style.opacity = 100;
+        targets[i].style.filter = "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+    }
+}
